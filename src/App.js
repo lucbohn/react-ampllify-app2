9be2e465990a96cc9f395a552342c724533cd0ea
unknown <lbohn@student.unimelb.edu.au>
2020-07-30 20:49:11 +1000

    Changed header name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1>Luc's Demo</h1>
+        <h1>JAMES's Demo</h1>
         </p>
         <a
           className="App-link"
